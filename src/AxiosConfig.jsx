9a4f3335673225a4b7d9b2126647e614a25326e3
bethelhemkidanu <betelkidanu@gmail.com>
2024-06-24 13:37:06 -0700

    deployment

--- src/AxiosConfig.jsx
+++ src/AxiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
  const axiosBase = axios.create({
-   baseURL: "https://evangadi-forum-backend-1-mlsi.onrender.com",
+   baseURL: "https://evangadi-forum-backend-1-mlsi.onrender.com/api",
    // baseURL: 'http://localhost:5500/api'
  });
  export default axiosBase
